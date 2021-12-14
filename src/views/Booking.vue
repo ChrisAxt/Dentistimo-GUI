b48b90bf6623167543497aa400601cca2091a877
“annseri” <annseri@student.chalmers.se>
2021-12-14 17:01:03 +0100

    Added updated timeslot view with hardcoded details for time and clinic

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -1,20 +1,40 @@
 <template>
 <div class="hi">
 </div>
-
-  <form>
-    <div class="date">
-  <label for="start">Please select a date and time for your appointment: &nbsp; &nbsp;
-  <input type="date" id="start" name="trip-start"
-         min="2018-01-01" max="2022-12-31" required>
-  <span class="validity"></span>
-  </label></div>
-  <div class="time">
-    <label for="time">Time: &nbsp;
-  <input type="time" id="start" name="trip-start"
-         value="2018-07-22"
-         min="2018-01-01" max="2018-12-31">
-         </label></div>
+  <div class="message">
+    <h3> Pick your time for appointment at Tooth Fairy Dentist</h3><br>
+</div>
+<div style="height:280px; width: 700px; position: center;">
+  <div class="days">
+  <div class="day">
+    <div class="datelabel"><strong>Monday</strong><br/>August 23</div>
+    <div class="timeslot">9:00am</div>
+    <div class="timeslot">9:30am</div>
+    <div class="timeslot">10:00am</div>
+  </div>
+  <div class="day">
+    <div class="datelabel"><strong>Tuesday</strong><br/>August 24</div>
+    <div class="timeslot">10:30pm</div>
+  </div>  
+  <div class="day">
+    <div class="datelabel"><strong>Wednesday</strong><br/>August 25</div>
+    <div class="timeslot">10:30pm</div>
+  </div>
+  <div class="day">
+    <div class="datelabel"><strong>Thursday</strong><br/>August 26</div>
+    <div class="timeslot">10:30pm</div>
+  </div>
+  <div class="day">
+    <div class="datelabel"><strong>Friday</strong><br/>August 27</div>
+    <div class="timeslot">10:30pm</div>
+  </div>
+  </div>
+  <div class="arrow-leftright"><i class='fas fa-angle-left' style='font-size:24px'></i>&nbsp;<i class='fas fa-angle-right' style='font-size:24px'></i></div>
+    <div class="selectedTime">
+    <h3> Selected time: 24 aug 10:30 am </h3><br>
+    <h4> Add contact details and click submit to confirm booking </h4><br>
+    </div>
+     <form>
       <div class="userInput">
 <label for="fname">First name:</label><br>
   <input type="text" id="fname" name="fname" value="" size="30"><br>
@@ -22,11 +42,13 @@
   <input type="text" id="emailadress" style="" name="lname" value="" size="30"><br>
   <label for="lname">Phone number:</label><br>
   <input type="text" id="pnumber" name="lname" value="" size="40" required><br>
-  </div>
-  <p>
+   <p id = "submit">
     <button>Submit</button>
   </p>
+  </div>
   </form>
+</div>
+
 </template>
 
 <script>
@@ -91,7 +113,21 @@ export default {
 </script>
 
 <style>
-
+h4{
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  font-size: 15px;
+  padding-left: 350px;
+}
+.arrow-leftright{
+  padding-left: 350px;
+}
+h3 {
+  font-weight: 500;
+  padding-top: 20px;
+  padding-left: 350px;
+}
 .date {
   display: flex;
   align-items: center;
@@ -136,8 +172,45 @@ p {
   background-image: linear-gradient(rgba(3, 3, 3, 0.7), rgba(3, 3, 3, 0.7)), url(../assets/smiling_woman_with_perfect_white_teeth-opt.png);
   background-size: cover;
   background-position: bottom;
-  height: 50vh;
+  height: 30vh;
   background-attachment: fixed;
   color: white;
+}
+
+.days {
+  width: 1100px;
+  padding-left: 350px;
+}
+
+.day {
+  width: 120px;
+  height: 230px;
+  background-color: #efeff6;
+  padding:10px;
+  float:left;
+  margin-right:7px;
+  margin-bottom:5px;
+}
+
+.datelabel {
+  margin-bottom: 15px;
+}
+
+.timeslot {
+  background-color: #00c09d;
+  width: auto;
+  height: 30px;
+  color: white;
+  padding:7px;
+  margin-top: 5px;
+  font-size: 14px;
+  border-radius: 3px;
+  vertical-align: center;
+  text-align:center;
+}
+
+.timeslot:hover { 
+  background-color: #2CA893;
+  cursor: pointer;
 }
 </style>
