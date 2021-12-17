66486f7475a68e5e33e7ac588799818aa808f2b6
axtc <axtc@student.chalmers.se>
2021-12-17 11:34:07 +0100

    Alternative deisgn for view timeslots (better to poplulate the list)

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -1,86 +1,225 @@
 <template>
-<div class="hi">
-</div>
-  <div class="message">
-    <h3> Pick your time for appointment at Tooth Fairy Dentist</h3><br>
-</div>
-<div style="height:280px; width: 700px; position: center;">
-  <div class="days">
-  <div class="day">
-    <div class="datelabel"><strong>Monday</strong><br/>December 13</div>
-    <div class="timeslot">9:00am</div>
-    <div class="timeslot">9:30am</div>
-    <div class="timeslot">10:00am</div>
-  </div>
-  <div class="day">
-    <div class="datelabel"><strong>Tuesday</strong><br/>December 14</div>
-    <div class="timeslot">10:30pm</div>
-  </div>  
-  <div class="day">
-    <div class="datelabel"><strong>Wednesday</strong><br/>December 15</div>
-    <div class="timeslot">10:30pm</div>
-  </div>
-  <div class="day">
-    <div class="datelabel"><strong>Thursday</strong><br/>December 16</div>
-    <div class="timeslot">10:30pm</div>
-  </div>
-  <div class="day">
-    <div class="datelabel"><strong>Friday</strong><br/>December 17</div>
-    <div class="timeslot">10:30pm</div>
-  </div>
-  </div>
-  <div class="arrow-leftright"><i class='fas fa-angle-left' style='font-size:24px'></i>&nbsp;<i class='fas fa-angle-right' style='font-size:24px'></i></div>
-    <div class="selectedTime">
-    <h3> Selected time: 24 aug 10:30 am </h3><br>
-    <h4> Add contact details and click submit to confirm booking </h4><br>
-    </div>
-     <form>
-      <div class="userInput">
-<label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value="" size="30"><br>
-  <label for="lname">Emailadress:</label><br>
-  <input type="text" id="emailadress" style="" name="lname" value="" size="30"><br>
-  <label for="lname">Phone number:</label><br>
-  <input type="text" id="pnumber" name="lname" value="" size="40" required><br>
-   <p id = "submit">
-    <button>Submit</button>
-  </p>
-  </div>
-  </form>
-</div>
+  <div class="hi"></div>
 
+  <div class="date" @change="onChangeDate($event)">
+    <label for="start"
+      >Please select a date and time for your appointment: &nbsp; &nbsp; <br><br>
+      <input
+        type="date"
+        id="start"
+        name="trip-start"
+        min="2018-01-01"
+        max="2022-12-31"
+        required
+      />
+      <span class="validity"></span>
+    </label>
+  </div>
+  <br />
+  <div>
+    <div class="time">
+      <select
+        name="time"
+        @change="onChangeTime($event)"
+        class="form-select form-control"
+      >
+        <option>-- Select Time --</option>
+        <option v-for="item in timeSlots" v-bind:key="item">
+          {{ item.start + "-" + item.end }}
+        </option>
+      </select>
+    </div>
+  </div>
+  <br />
+  <form>
+    <div class="userInput">
+      <label for="fname">First name:</label><br />
+      <input type="text" id="fname" name="fname" value="" size="30" /><br />
+      <label for="lname">Emailadress:</label><br />
+      <input
+        type="text"
+        id="emailadress"
+        style=""
+        name="lname"
+        value=""
+        size="30"
+      /><br />
+      <label for="lname">Phone number:</label><br />
+      <input
+        type="text"
+        id="pnumber"
+        name="lname"
+        value=""
+        size="40"
+        required
+      /><br />
+      <p id="submit">
+        <button type='button' v-onclick="booking">This Button</button>
+        <!-- <button>Submit</button> -->
+      </p>
+    </div>
+  </form>
 </template>
 
 <script>
-import mqtt from "mqtt"
+import mqtt from "mqtt";
 
 export default {
   mounted() {
-    console.log('mounted')
-    this.createConnection()
+    console.log("mounted");
+    this.createConnection();
   },
   data() {
     return {
       connection: {
-        host: '127.0.0.1',
+        host: "127.0.0.1",
         port: 9001,
-        endpoint: '/mqtt',
+        endpoint: "/mqtt",
         clean: true, // Reserved session
         // Certification Information
-        clientId: 'Dentistimo Team5 - Client n°' + Math.random().toString(16).substr(2, 8)
+        clientId:
+          "Dentistimo Team5 - Client n°" +
+          Math.random().toString(16).substr(2, 8),
       },
       qosList: [
-        {label: 0, value: 0},
-        {label: 1, value: 1},
-        {label: 2, value: 2},
+        { label: 0, value: 0 },
+        { label: 1, value: 1 },
+        { label: 2, value: 2 },
       ],
       client: {
         connected: false,
       },
       subscribeSuccess: false,
-    }
+      onChangeTime(e) {
+        console.log(e.target.value);
+      },
+      onChangeDate(e) {
+        console.log(e.target.value);
+        /* var mqttPayload;
+        client.publish("/Team5/Dentistimo/GenerateTimeSlots", mqttPayload, {
+          qos: 1,
+        }); */
+      },
+      timeSlots: [
+        {
+          _id: "61b99f40a8ef5bf7d53703b4",
+          start: "9:00",
+          end: "9:30",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703b5",
+          start: "9:30",
+          end: "10:00",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703b6",
+          start: "10:00",
+          end: "10:30",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703b7",
+          start: "10:30",
+          end: "11:00",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703b8",
+          start: "11:00",
+          end: "11:30",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703b9",
+          start: "11:30",
+          end: "12:00",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703ba",
+          start: "12:00",
+          end: "12:30",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703bb",
+          start: "12:30",
+          end: "13:00",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703bc",
+          start: "13:00",
+          end: "13:30",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703bd",
+          start: "13:30",
+          end: "14:00",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703be",
+          start: "14:00",
+          end: "14:30",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703bf",
+          start: "14:30",
+          end: "15:00",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703c0",
+          start: "15:00",
+          end: "15:30",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703c1",
+          start: "15:30",
+          end: "16:00",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703c2",
+          start: "16:00",
+          end: "16:30",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+        {
+          _id: "61b99f40a8ef5bf7d53703c3",
+          start: "16:30",
+          end: "17:00",
+          available: 3,
+          date: "Thu Dec 02 2021",
+        },
+      ],
+    };
   },
   methods: {
+    booking(){
+      alert('inside booking')
+    },
     // Create connection
     createConnection() {
       // Connect string, and specify the connection method used through protocol
@@ -90,37 +229,43 @@ export default {
       // mqtts encrypted TCP connection
       // wxs WeChat mini app connection
       // alis Alipay mini app connection
-      const {host, port, endpoint, ...options} = this.connection
-      const connectUrl = `ws://${host}:${port}${endpoint}`
+      const { host, port, endpoint, ...options } = this.connection;
+      const connectUrl = `ws://${host}:${port}${endpoint}`;
       try {
-        this.client = mqtt.connect(connectUrl, options)
+        this.client = mqtt.connect(connectUrl, options);
       } catch (error) {
-        console.log('mqtt.connect error', error)
+        console.log("mqtt.connect error", error);
       }
-      this.client.on('connect', () => {
-        console.log('Connection succeeded!')
+      this.client.on("connect", () => {
+        console.log("Connection succeeded!");
         //this.client.subscribe() //TODO: Define which topics to subscribe to for this page
-      })
-      this.client.on('error', error => {
-        console.log('Connection failed', error)
-      })
-      this.client.on('message', (topic, message) => {
-        //TODO: Describe reaction to message here: process data and store it into an object in data()
-      })
-    }
-  }
-}
+      });
+      this.client.on("error", (error) => {
+        console.log("Connection failed", error);
+      });
+      this.client.on("message", (topic, message) => {
+        if (topic == "/Team5/Dentistimo/TimeSlots") {
+          try {
+            this.timeSlots = JSON.parse(message);
+          } catch (error) {
+            console.log(error)
+          }
+        }
+      });
+    },
+  },
+};
 </script>
 
 <style>
-h4{
+h4 {
   font-weight: 400;
   display: flex;
   align-items: center;
   font-size: 15px;
   padding-left: 350px;
 }
-.arrow-leftright{
+.arrow-leftright {
   padding-left: 350px;
 }
 h3 {
@@ -142,24 +287,24 @@ input {
   align-items: center;
   padding-top: 0px;
   padding-left: 350px;
+  width: fit-content;
 }
 
- .userInput {
+.userInput {
   padding-left: 350px;
 }
-
 
 span::after {
   padding-left: 5px;
 }
 
 input:invalid + span::after {
-  content: '✖';
+  content: "✖";
   color: red;
 }
 
-input:valid+span::after {
-  content: '✓';
+input:valid + span::after {
+  content: "✓";
   color: #8cc63f;
 }
 
@@ -168,8 +313,9 @@ p {
   padding-top: 40px;
   padding-left: 350px;
 }
-.hi{
-  background-image: linear-gradient(rgba(3, 3, 3, 0.7), rgba(3, 3, 3, 0.7)), url(../assets/smiling_woman_with_perfect_white_teeth-opt.png);
+.hi {
+  background-image: linear-gradient(rgba(3, 3, 3, 0.7), rgba(3, 3, 3, 0.7)),
+    url(../assets/smiling_woman_with_perfect_white_teeth-opt.png);
   background-size: cover;
   background-position: bottom;
   height: 30vh;
@@ -186,10 +332,10 @@ p {
   width: 120px;
   height: 230px;
   background-color: #f4f7ff;
-  padding:10px;
-  float:left;
-  margin-right:7px;
-  margin-bottom:5px;
+  padding: 10px;
+  float: left;
+  margin-right: 7px;
+  margin-bottom: 5px;
 }
 
 .datelabel {
@@ -203,15 +349,15 @@ p {
   height: 30px;
   color: rgb(49, 88, 138);
   font-weight: 500;
-  padding:7px;
+  padding: 7px;
   margin-top: 5px;
   font-size: 14px;
   border-radius: 3px;
   vertical-align: center;
-  text-align:center;
+  text-align: center;
 }
 
-.timeslot:hover { 
+.timeslot:hover {
   background-color: #3d83d3;
   cursor: pointer;
 }
